cb3521a1a28333ba8adc5acfae90abaa4c907814
Igor Rodionov <goruha@gmail.com>
2025-06-01 00:30:37 +0300

    Update cloudtrail.tf

--- src/cloudtrail.tf
+++ src/cloudtrail.tf
@@ -7,7 +7,7 @@ locals {
   cloudtrail_table_name = "%s_cloudtrail_logs"
 
   # s3://cloudtrail_bucket_name/AWSLogs/organization_id/Account_ID/CloudTrail/
-  organization_id         = module.account_map.outputs.org.id
+  organization_id = module.account_map.outputs.org.id
   filtered_full_account_map = var.account_map_exclusion_regex == "" ? module.account_map.outputs.full_account_map : { 
     for k, v in module.account_map.outputs.full_account_map : 
     k => v 
